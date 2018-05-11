e622118be0464be641675881cbddbc3e39c227ff
Vicki Hamm <vicki.cello@gmail.com>
2018-05-11 00:07:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
   board.each do |board|
-    if board == "X" || "O"
+    if board == "X" || board == "O"
     counter += 1
   end
 end
